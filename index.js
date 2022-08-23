cc23b6b941c94ee21cc99291bd7036099c307757
max-marschhauser <max.marschhauser@gmail.com>
2022-08-23 20:05:42 +0200

    finished
    finished

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
             entry.target.classList.toggle("show", entry.isIntersecting);
-            /*if (entry.isIntersecting) observer.unobserve(entry.target);*/ //ako želim da element koji je jednom došao na ekran tako i ostame i kada se ponovno skrolla gore
+            /*if (entry.isIntersecting) observer.unobserve(entry.target);*/ // Element that has once entered the site will remain seen, will not reenter after scrolling back up
         });
     },
     {
-        root: null, //it is the viewport, default, najčešće ne dirati
-        threshold: 1, //koji postotak novog elementa mora biti na stanici prije nego što uđe
-        rootMargin: "-100px 0px -100px 0px", //margina na viewportu
+        root: null, //it is the viewport, default, mostly do not touch
+        threshold: 1, //what percent of element must be on the site before it is shown
+        rootMargin: "-100px 0px -100px 0px", //margina on viewport
     }
 );
 
